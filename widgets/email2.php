2419d48075629c56074f497014ed576b62aa9bfa
Bill Newman <william.newman@seattlecolleges.edu>
2018-02-21 18:02:52 -0800

    Adds initial widget folder
    This is prior to any theming and discludes the credentials.php file.

---
+++ widgets/email2.php
@@ -0,0 +1,36 @@
+<?php
+//email2.php
+
+if(isset($_POST['Submit']))
+{//send email?
+    /*
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+    */
+    
+    $to = "wnewma01@seattlecentral.edu";
+    
+    $subject = "My WebSite Feedback " . date("m/d/y, G:i:s");
+    
+    $text = '';//initialize variable
+    
+    $text .= 'Name: ' . $_POST['Name'] . PHP_EOL . PHP_EOL;
+    $text .= 'Email: ' . $_POST['Email'] . PHP_EOL . PHP_EOL;
+    $text .= 'Comments: ' . $_POST['Comments'] . PHP_EOL . PHP_EOL;
+    
+    mail($to,$subject,$text);
+    
+    echo "email sent?";
+
+}else{//show form!
+    echo '
+    <form action="" method="post">
+    Name: <input type="text" name="Name" /><br />
+    Email: <input type="email" name="Email" /><br />
+    Comments: <textarea name="Comments"></textarea><br />
+    <input type="submit" name="Submit" />
+    </form>
+    ';
+
+}
---
+++ widgets/template.php
@@ -0,0 +1,34 @@
+<?php include 'includes/config.php'?>
+<?php include 'includes/header.php'?>    
+<h2>Our Template</h2>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<?php include 'includes/footer.php'?>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
